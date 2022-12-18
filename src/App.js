7af1816307f1355ed43c4915fc7449556cb4e2e2
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-18 20:20:45 +0400

    added customHook useGame for "business logic"

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import "./App.css";
+import useGame from "./useGame";
 
 function App() {
-  const STARTING_TIME = 5;
-
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
-  const [wordСount, setWordСount] = useState(0);
-  const [start, setStart] = useState(false);
-
-  const textareaRef = useRef(null);
-
-  function handleChange(event) {
-    setText(event.target.value);
-  }
-
-  function countWords() {
-    const wordsArray = text.trim().split(" ");
-    return wordsArray.filter((word) => word !== "").length;
-  }
-
-  useEffect(() => {
-    if (timeRemaining > 0 && start) {
-      setTimeout(() => {
-        setTimeRemaining((prevTime) => prevTime - 1);
-      }, 1000);
-    }
-    if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, start]);
-
-  function startGame() {
-    setStart(true);
-    setTimeRemaining(STARTING_TIME);
-    setText("");
-    setWordСount(0);
-    textareaRef.current.disabled = false;
-    textareaRef.current.focus();
-  }
-
-  function endGame() {
-    setStart(false);
-    setWordСount(countWords());
-  }
+  const { textareaRef, handleChange, text, start, timeRemaining, startGame, wordСount } =
+    useGame(20);
 
   return (
     <main>
---
+++ src/useGame.js
@@ -0,0 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+
+function useGame(startingTime = 15) {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(startingTime);
+  const [wordСount, setWordСount] = useState(0);
+  const [start, setStart] = useState(false);
+
+  const textareaRef = useRef(null);
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function countWords() {
+    const wordsArray = text.trim().split(" ");
+    return wordsArray.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && start) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+    if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, start]);
+
+  function startGame() {
+    setStart(true);
+    setTimeRemaining(startingTime);
+    setText("");
+    setWordСount(0);
+    textareaRef.current.disabled = false;
+    textareaRef.current.focus();
+  }
+
+  function endGame() {
+    setStart(false);
+    setWordСount(countWords());
+  }
+
+  return { textareaRef, handleChange, text, start, timeRemaining, startGame, wordСount };
+}
+
+export default useGame;
