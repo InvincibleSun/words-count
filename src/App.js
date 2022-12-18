06a209a46c4947009179b888235a5faeaebde216
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-18 20:35:17 +0400

    added Text as a sample

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
   return (
     <main>
       <h1>How fast do you type?</h1>
+      <div>
+        <p>Try to type these words as many times as possible in 20 sec</p>
+        <p>Russia is a terrorist state</p>
+      </div>
       <textarea value={text} onChange={handleChange} disabled={!start} ref={textareaRef} />
       <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={startGame} disabled={start}>
